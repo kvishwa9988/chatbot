971c7d681370848327f1a9f153fdc6b5bdf0ef58
kvishwa9988 <kvishwa9988@gmail.com>
2026-02-05 16:53:03 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -32,7 +32,7 @@ if prompt := st.chat_input("Ask.."):
                 "message": prompt,
                 "session_id": st.session_state.session_id
             }
-            response = requests.post("https://chatbot-1-6wzd.onrender.com", json=payload)
+            response = requests.post("https://chatbot-1-6wzd.onrender.com/chat", json=payload)
             
             if response.status_code == 200:
                 bot_response = response.json()["output"]
@@ -46,4 +46,5 @@ if prompt := st.chat_input("Ask.."):
         except Exception as e:
 
             st.error(f"Failed to connect to backend: {e}")
+
 
